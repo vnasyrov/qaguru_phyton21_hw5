7ba1f434b1e48517e4f7602990363c0cd9f46a78
Valeriy Nasyrov <valerius.nasyrov@yandex.ru>
2025-08-21 21:51:38 +0300

    Добавил файл html + написал код до заполнения формы даты рождения

--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,4 +1,21 @@
-from selene import browser
+from selene import browser, have, command, be
 
 def test_submit_form():
-    browser.open()
+
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.element("#firstName").type('Валерий').press_enter()
+    browser.element("#lastName").type('Насыров').press_enter()
+    browser.element("#userEmail").type('user@example.com').press_enter()
+    browser.element('label[for="gender-radio-1"]').click()
+    browser.element("#userNumber").type('8999999999').press_enter()
+    #browser.element("#dateOfBirthInput").type('12 Jul 1993').press_enter()
+    browser.element('#dateOfBirthInput').perform(command.js.click)
+    browser.element('.react-datepicker__month-select').element('option[value="6"]').click()
+    browser.element('.react-datepicker__year-select').element('option[value="1993"]').click()
+    browser.element('.react-datepicker__day--012').perform(command.js.click)
+
+
+pass
